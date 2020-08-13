2816af038d04b4aa1a8652bcd87951e68a8da85a
harshver <verharsh@iitk.ac.in>
2020-08-13 06:05:16 +0530

    3rd

---
+++ rankhospital.R
@@ -0,0 +1,23 @@
+rankhospital<-function(state,outcome,num){
+     setwd("C:/Users/hp/Desktop/R/PA3/rprog_data_ProgAssignment3-data")
+     oc<-read.csv("outcome-of-care-measures.csv",colClasses="character")
+     setwd("C:/Users/hp/Desktop/R/PA3")
+     if(sum(oc$State==state)==0){
+          stop("invalid state")
+     }
+     oc<-oc[oc$State==state,]
+     find<-which(c("heart attack","heart failure","pneumonia")==outcome,arr.ind=TRUE)*6+5
+     if(length(find)==0){
+          stop("invalid outcome")
+     }
+     oc<-data.frame(Hospital_Name=oc[,2],out=oc[,find])
+     oc<-oc[!oc$out=="Not Available",]
+     
+     
+     
+     
+     
+     
+     
+     
+}
